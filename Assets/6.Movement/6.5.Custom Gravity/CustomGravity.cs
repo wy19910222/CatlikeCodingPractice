5f42bd99b999263e8908907193e254696bd190dc
yun.wang <wy1991022219910222@163.com>
2024-08-15 02:38:31 +0800

    6.5.Custom Gravity

---
+++ Assets/6.Movement/6.5.Custom Gravity/CustomGravity.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace CustomGravity {
+	public static class CustomGravity {
+		public static Vector3 GetGravity(Vector3 position) {
+			return position.normalized * Physics.gravity.y;
+			// return Physics.gravity;
+		}
+		
+		public static Vector3 GetUpAxis(Vector3 position) {
+			Vector3 up = position.normalized;
+			return Physics.gravity.y < 0f ? up : -up;
+			// return -Physics.gravity.normalized;
+		}
+		
+		public static Vector3 GetGravity(Vector3 position, out Vector3 upAxis) {
+			upAxis = GetUpAxis(position);
+			return GetGravity(position);
+		}
+	}
+}
---
+++ Assets/6.Movement/6.5.Custom Gravity/CustomGravityRigidbody.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace CustomGravity {
+	[RequireComponent(typeof(Rigidbody))]
+	public class CustomGravityRigidbody : MonoBehaviour {
+		[SerializeField] private bool floatToSleep;
+		
+		private Rigidbody body;
+		private float floatDelay;
+
+		private void Awake () {
+			body = GetComponent<Rigidbody>();
+			body.useGravity = false;
+		}
+
+		private void FixedUpdate() {
+			if (floatToSleep) {
+				if (body.IsSleeping()) {
+					floatDelay = 0f;
+					return;
+				}
+				if (body.velocity.sqrMagnitude < 0.0001f) {
+					floatDelay += Time.deltaTime;
+					if (floatDelay >= 1f) {
+						return;
+					}
+				} else {
+					floatDelay = 0f;
+				}
+			}
+			body.AddForce(CustomGravity.GetGravity(body.position), ForceMode.Acceleration);
+		}
+	}
+}
