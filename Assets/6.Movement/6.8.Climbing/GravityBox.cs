6e5a6cd1c9a2c6cf766c92f03e7db24580437305
yun.wang <wy1991022219910222@163.com>
2024-08-20 14:36:34 +0800

    6.8.Climbing

---
+++ Assets/6.Movement/6.8.Climbing/GravityBox.cs
@@ -0,0 +1,312 @@
+﻿using UnityEngine;
+
+namespace Climbing {
+	public class GravityBox : GravitySource {
+		[SerializeField] private float gravity = 9.81f;
+		[SerializeField] private Vector3 boundaryDistance = Vector3.one;
+		[SerializeField, Min(0f)] private float innerDistance, innerFalloffDistance;
+		[SerializeField, Min(0f)] private float outerDistance, outerFalloffDistance;
+
+		private float innerFalloffFactor, outerFalloffFactor;
+
+		public override Vector3 GetGravity(Vector3 position) {
+			position = transform.InverseTransformDirection(position - transform.position);
+			Vector3 vector = Vector3.zero;
+			int outside = 0;
+			if (position.x > boundaryDistance.x) {
+				vector.x = boundaryDistance.x - position.x;
+				outside = 1;
+			}
+			else if (position.x < -boundaryDistance.x) {
+				vector.x = -boundaryDistance.x - position.x;
+				outside = 1;
+			}
+
+			if (position.y > boundaryDistance.y) {
+				vector.y = boundaryDistance.y - position.y;
+				outside += 1;
+			}
+			else if (position.y < -boundaryDistance.y) {
+				vector.y = -boundaryDistance.y - position.y;
+				outside += 1;
+			}
+
+			if (position.z > boundaryDistance.z) {
+				vector.z = boundaryDistance.z - position.z;
+				outside += 1;
+			}
+			else if (position.z < -boundaryDistance.z) {
+				vector.z = -boundaryDistance.z - position.z;
+				outside += 1;
+			}
+			
+			if (outside > 0) {
+				float distance = outside == 1 ? Mathf.Abs(vector.x + vector.y + vector.z) : vector.magnitude;
+				if (distance > outerFalloffDistance) {
+					return Vector3.zero;
+				}
+				float g = gravity / distance;
+				if (distance > outerDistance) {
+					g *= 1f - (distance - outerDistance) * outerFalloffFactor;
+				}
+				return transform.TransformDirection(g * vector);
+			}
+			
+			Vector3 distances;
+			distances.x = boundaryDistance.x - Mathf.Abs(position.x);
+			distances.y = boundaryDistance.y - Mathf.Abs(position.y);
+			distances.z = boundaryDistance.z - Mathf.Abs(position.z);
+			if (distances.x < distances.y) {
+				if (distances.x < distances.z) {
+					vector.x = GetGravityComponent(position.x, distances.x);
+				}
+				else {
+					vector.z = GetGravityComponent(position.z, distances.z);
+				}
+			}
+			else if (distances.y < distances.z) {
+				vector.y = GetGravityComponent(position.y, distances.y);
+			}
+			else {
+				vector.z = GetGravityComponent(position.z, distances.z);
+			}
+			return transform.TransformDirection(vector);
+		}
+
+		private float GetGravityComponent(float coordinate, float distance) {
+			if (distance > innerFalloffDistance) {
+				return 0f;
+			}
+			float g = gravity;
+			if (distance > innerDistance) {
+				g *= 1f - (distance - innerDistance) * innerFalloffFactor;
+			}
+			return coordinate > 0f ? -g : g;
+		}
+
+		private void Awake() {
+			OnValidate();
+		}
+
+		private void OnValidate() {
+			boundaryDistance = Vector3.Max(boundaryDistance, Vector3.zero);
+			float maxInner = Mathf.Min(Mathf.Min(boundaryDistance.x, boundaryDistance.y), boundaryDistance.z);
+			innerDistance = Mathf.Min(innerDistance, maxInner);
+			innerFalloffDistance = Mathf.Max(Mathf.Min(innerFalloffDistance, maxInner), innerDistance);
+			outerFalloffDistance = Mathf.Max(outerFalloffDistance, outerDistance);
+			
+			innerFalloffFactor = 1f / (innerFalloffDistance - innerDistance);
+			outerFalloffFactor = 1f / (outerFalloffDistance - outerDistance);
+		}
+
+		private void OnDrawGizmos() {
+			Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+			Vector3 size;
+			if (innerFalloffDistance > innerDistance) {
+				Gizmos.color = Color.cyan;
+				size.x = 2f * (boundaryDistance.x - innerFalloffDistance);
+				size.y = 2f * (boundaryDistance.y - innerFalloffDistance);
+				size.z = 2f * (boundaryDistance.z - innerFalloffDistance);
+				Gizmos.DrawWireCube(Vector3.zero, size);
+			}
+			if (innerDistance > 0f) {
+				Gizmos.color = Color.yellow;
+				size.x = 2f * (boundaryDistance.x - innerDistance);
+				size.y = 2f * (boundaryDistance.y - innerDistance);
+				size.z = 2f * (boundaryDistance.z - innerDistance);
+				Gizmos.DrawWireCube(Vector3.zero, size);
+			}
+			Gizmos.color = Color.red;
+			Gizmos.DrawWireCube(Vector3.zero, 2f * boundaryDistance);
+			
+			if (outerDistance > 0f) {
+				Gizmos.color = Color.yellow;
+				// DrawGizmosOuterCube(outerDistance);
+				DrawExtendedWireCube(Vector3.zero, boundaryDistance * 2, outerDistance);
+			}
+			if (outerFalloffDistance > outerDistance) {
+				Gizmos.color = Color.cyan;
+				// DrawGizmosOuterCube(outerFalloffDistance);
+				DrawExtendedWireCube(Vector3.zero, boundaryDistance * 2, outerFalloffDistance);
+			}
+		}
+
+		private void DrawGizmosOuterCube(float distance) {
+			Vector3 a, b, c, d;
+			a.y = b.y = boundaryDistance.y;
+			d.y = c.y = -boundaryDistance.y;
+			b.z = c.z = boundaryDistance.z;
+			d.z = a.z = -boundaryDistance.z;
+			a.x = b.x = c.x = d.x = boundaryDistance.x + distance;
+			DrawGizmosRect(a, b, c, d);
+			a.x = b.x = c.x = d.x = -a.x;
+			DrawGizmosRect(a, b, c, d);
+
+			a.x = d.x = boundaryDistance.x;
+			b.x = c.x = -boundaryDistance.x;
+			a.z = b.z = boundaryDistance.z;
+			c.z = d.z = -boundaryDistance.z;
+			a.y = b.y = c.y = d.y = boundaryDistance.y + distance;
+			DrawGizmosRect(a, b, c, d);
+			a.y = b.y = c.y = d.y = -a.y;
+			DrawGizmosRect(a, b, c, d);
+
+			a.x = d.x = boundaryDistance.x;
+			b.x = c.x = -boundaryDistance.x;
+			a.y = b.y = boundaryDistance.y;
+			c.y = d.y = -boundaryDistance.y;
+			a.z = b.z = c.z = d.z = boundaryDistance.z + distance;
+			DrawGizmosRect(a, b, c, d);
+			a.z = b.z = c.z = d.z = -a.z;
+			DrawGizmosRect(a, b, c, d);
+			
+			distance *= 0.5773502692f;
+			Vector3 size = boundaryDistance;
+			size.x = 2f * (size.x + distance);
+			size.y = 2f * (size.y + distance);
+			size.z = 2f * (size.z + distance);
+			Gizmos.DrawWireCube(Vector3.zero, size);
+		}
+
+		private void DrawGizmosRect(Vector3 a, Vector3 b, Vector3 c, Vector3 d) {
+			Gizmos.DrawLine(a, b);
+			Gizmos.DrawLine(b, c);
+			Gizmos.DrawLine(c, d);
+			Gizmos.DrawLine(d, a);
+		}
+
+		private static void DrawExtendedWireCube(Vector3 center, Vector3 size, float extent, bool detailed = false) {
+			if (extent <= 0) {
+				Gizmos.DrawWireCube(center, size);
+				return;
+			}
+			size *= 0.5F;
+			
+			// 6个面
+			Vector3 a, b, c, d;
+			a.y = b.y = size.y;
+			d.y = c.y = -size.y;
+			b.z = c.z = size.z;
+			d.z = a.z = -size.z;
+			a.x = b.x = c.x = d.x = size.x + extent;
+			Gizmos.DrawLine(a, b);
+			Gizmos.DrawLine(b, c);
+			Gizmos.DrawLine(c, d);
+			Gizmos.DrawLine(d, a);
+			a.x = b.x = c.x = d.x = -a.x;
+			Gizmos.DrawLine(a, b);
+			Gizmos.DrawLine(b, c);
+			Gizmos.DrawLine(c, d);
+			Gizmos.DrawLine(d, a);
+			a.x = b.x = size.x;
+			d.x = c.x = -size.x;
+			b.z = c.z = size.z;
+			d.z = a.z = -size.z;
+			a.y = b.y = c.y = d.y = size.y + extent;
+			Gizmos.DrawLine(a, b);
+			Gizmos.DrawLine(b, c);
+			Gizmos.DrawLine(c, d);
+			Gizmos.DrawLine(d, a);
+			a.y = b.y = c.y = d.y = -a.y;
+			Gizmos.DrawLine(a, b);
+			Gizmos.DrawLine(b, c);
+			Gizmos.DrawLine(c, d);
+			Gizmos.DrawLine(d, a);
+			a.x = b.x = size.x;
+			d.x = c.x = -size.x;
+			b.y = c.y = size.y;
+			d.y = a.y = -size.y;
+			a.z = b.z = c.z = d.z = size.z + extent;
+			Gizmos.DrawLine(a, b);
+			Gizmos.DrawLine(b, c);
+			Gizmos.DrawLine(c, d);
+			Gizmos.DrawLine(d, a);
+			a.z = b.z = c.z = d.z = -a.z;
+			Gizmos.DrawLine(a, b);
+			Gizmos.DrawLine(b, c);
+			Gizmos.DrawLine(c, d);
+			Gizmos.DrawLine(d, a);
+
+			// 8个角上的圆弧三角形
+			Vector3 cornerCenter = size;
+			Vector3[] cornerTypes = {
+				Vector3.one,
+				new Vector3(-1, 1, 1),
+				new Vector3(1, -1, 1),
+				new Vector3(-1, -1, 1),
+				new Vector3(1, 1, -1),
+				new Vector3(-1, 1, -1),
+				new Vector3(1, -1, -1),
+				-Vector3.one,
+			};
+			const int ARC_SEGMENT_COUNT = 10;
+			for (int axis = 0; axis < 3; ++axis) {
+				Vector3 rotateAxis = Vector3.zero;
+				rotateAxis[axis] = 1;
+				Vector3 direction = Vector3.zero;
+				direction[axis == 2 ? 0 : axis + 1] = extent;
+				Vector3 temp1 = cornerCenter + direction;
+				for (int i = 1; i <= ARC_SEGMENT_COUNT; ++i) {
+					Vector3 temp2 = cornerCenter + Quaternion.AngleAxis(i * 90F / ARC_SEGMENT_COUNT, rotateAxis) * direction;
+					for (int j = 0; j < 8; ++j) {
+						Gizmos.DrawLine(Vector3.Scale(temp1, cornerTypes[j]), Vector3.Scale(temp2, cornerTypes[j]));
+					}
+					temp1 = temp2;
+				}
+			}
+
+			if (detailed) {
+				const float INV_SQRT_2 = 0.7071F;
+				float extent2 = extent * INV_SQRT_2;
+				
+				// 12条棱
+				Vector3 e, f;
+				a.x = -size.x;
+				b.x = size.x;
+				c.x = d.x = e.x = f.x = size.x + extent2;
+				c.y = -size.y;
+				d.y = size.y;
+				a.y = b.y = e.y = f.y = size.y + extent2;
+				e.z = -size.z;
+				f.z = size.z;
+				a.z = b.z = c.z = d.z = size.z + extent2;
+				Gizmos.DrawLine(a, b);
+				Gizmos.DrawLine(c, d);
+				Gizmos.DrawLine(e, f);
+				c.x = d.x = e.x = f.x = -c.x;
+				a.z = b.z = c.z = d.z = -a.z;
+				Gizmos.DrawLine(a, b);
+				Gizmos.DrawLine(c, d);
+				Gizmos.DrawLine(e, f);
+				c.x = d.x = e.x = f.x = -c.x;
+				a.y = b.y = e.y = f.y = -a.y;
+				Gizmos.DrawLine(a, b);
+				Gizmos.DrawLine(c, d);
+				Gizmos.DrawLine(e, f);
+				c.x = d.x = e.x = f.x = -c.x;
+				a.z = b.z = c.z = d.z = -a.z;
+				Gizmos.DrawLine(a, b);
+				Gizmos.DrawLine(c, d);
+				Gizmos.DrawLine(e, f);
+				
+				// 8个角上的圆弧棱
+				for (int axis = 0; axis < 3; ++axis) {
+					Vector3 rotateAxis = Vector3.zero;
+					rotateAxis[axis] = INV_SQRT_2;
+					rotateAxis[axis == 2 ? 0 : axis + 1] = -INV_SQRT_2;
+					Vector3 direction = Vector3.zero;
+					direction[axis] = extent2;
+					direction[axis == 2 ? 0 : axis + 1] = extent2;
+					Vector3 temp1 = cornerCenter + direction;
+					for (int i = 1; i <= ARC_SEGMENT_COUNT; ++i) {
+						Vector3 temp2 = cornerCenter + Quaternion.AngleAxis(i * 90F / ARC_SEGMENT_COUNT, rotateAxis) * direction;
+						for (int j = 0; j < 8; ++j) {
+							Gizmos.DrawLine(Vector3.Scale(temp1, cornerTypes[j]), Vector3.Scale(temp2, cornerTypes[j]));
+						}
+						temp1 = temp2;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/6.Movement/6.8.Climbing/MovingSphere.cs
@@ -0,0 +1,288 @@
+using UnityEngine;
+
+namespace Climbing {
+	public class MovingSphere : MonoBehaviour {
+		private static readonly int baseColorId = Shader.PropertyToID("_BaseColor");
+		
+		[SerializeField] private Transform playerInputSpace;
+		[SerializeField, Range(0f, 100f)] private float maxSpeed = 10f, maxClimbSpeed = 4f;
+		[SerializeField, Range(0f, 100f)] private float maxAcceleration = 10f, maxAirAcceleration = 1f, maxClimbAcceleration = 40f;
+		[SerializeField, Range(0f, 10f)] private float jumpHeight = 2f;
+		[SerializeField, Range(0, 5)] private int maxAirJumps;
+		[SerializeField, Range(0f, 90f)] private float maxGroundAngle = 25f, maxStairsAngle = 50f;
+		[SerializeField, Range(90, 180)] private float maxClimbAngle = 140f;
+		[SerializeField, Range(0f, 100f)] private float maxSnapSpeed = 100f;
+		[SerializeField, Min(0f)] private float probeDistance = 1f;
+		[SerializeField] private LayerMask probeMask = -1, stairsMask = -1, climbMask = -1;
+		[SerializeField] private Material normalMaterial, climbingMaterial;
+
+		private Vector2 playerInput;
+		private Vector3 velocity, connectionVelocity;
+		private Rigidbody body, connectedBody, previousConnectedBody;
+		private bool desiredJump, desiresClimbing;
+		private int groundContactCount, steepContactCount, climbContactCount;
+		private int jumpPhase;
+		private float minGroundDotProduct, minStairsDotProduct, minClimbDotProduct;
+		private Vector3 contactNormal, steepNormal, climbNormal, lastClimbNormal;
+		private int stepsSinceLastGrounded, stepsSinceLastJump;
+		private Vector3 upAxis, rightAxis, forwardAxis;
+		private Vector3 connectionWorldPosition, connectionLocalPosition;
+		private MeshRenderer meshRenderer;
+		
+		private bool OnGround => groundContactCount > 0;
+		private bool OnSteep => steepContactCount > 0;
+		private bool Climbing => climbContactCount > 0 && stepsSinceLastJump > 2;
+
+		private void OnValidate () {
+			minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+			minStairsDotProduct = Mathf.Cos(maxStairsAngle * Mathf.Deg2Rad);
+			minClimbDotProduct = Mathf.Cos(maxClimbAngle * Mathf.Deg2Rad);
+		}
+
+		private void Awake() {
+			body = GetComponent<Rigidbody>();
+			body.useGravity = false;
+			meshRenderer = GetComponent<MeshRenderer>();
+			OnValidate();
+		}
+
+		private void Update() {
+			playerInput.x = Input.GetAxis("Horizontal");
+			playerInput.y = Input.GetAxis("Vertical");
+			playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+			if (playerInputSpace) {
+				rightAxis = ProjectDirectionOnPlane(playerInputSpace.right, upAxis);
+				forwardAxis = ProjectDirectionOnPlane(playerInputSpace.forward, upAxis);
+			} else {
+				rightAxis = ProjectDirectionOnPlane(Vector3.right, upAxis);
+				forwardAxis = ProjectDirectionOnPlane(Vector3.forward, upAxis);
+			}
+			desiredJump |= Input.GetButtonDown("Jump");
+			desiresClimbing = Input.GetButton("Climb");
+			GetComponent<Renderer>().material.SetColor(baseColorId, Color.white * (groundContactCount * 0.25f));
+			
+			meshRenderer.material = Climbing ? climbingMaterial : normalMaterial;
+		}
+		
+		private void FixedUpdate() {
+			Vector3 gravity = CustomGravity.GetGravity(body.position, out upAxis);
+			UpdateState();
+			AdjustVelocity();
+			if (desiredJump) {
+				desiredJump = false;
+				Jump(gravity);
+			}
+			if (Climbing) {
+				velocity -= contactNormal * (maxClimbAcceleration * 0.9f * Time.deltaTime);
+			} else if (OnGround && velocity.sqrMagnitude < 0.01f) {
+				velocity += contactNormal * (Vector3.Dot(gravity, contactNormal) * Time.deltaTime);
+			} else if (desiresClimbing && OnGround) {
+				velocity += (gravity - contactNormal * (maxClimbAcceleration * 0.9f)) * Time.deltaTime;
+			} else {
+				velocity += gravity * Time.deltaTime;
+			}
+			body.velocity = velocity;
+			ClearState();
+		}
+
+		private void ClearState() {
+			groundContactCount = steepContactCount = climbContactCount = 0;
+			contactNormal = steepNormal = climbNormal = Vector3.zero;
+			connectionVelocity = Vector3.zero;
+			previousConnectedBody = connectedBody;
+			connectedBody = null;
+		}
+
+		private void UpdateState() {
+			stepsSinceLastGrounded += 1;
+			stepsSinceLastJump += 1;
+			velocity = body.velocity;
+			if (CheckClimbing() || OnGround || SnapToGround() || CheckSteepContacts()) {
+				stepsSinceLastGrounded = 0;
+				if (stepsSinceLastJump > 1) {
+					jumpPhase = 0;
+				}
+				if (groundContactCount > 1) {
+					contactNormal.Normalize();
+				}
+			} else {
+				contactNormal = upAxis;
+			}
+			if (connectedBody) {
+				if (connectedBody.isKinematic || connectedBody.mass >= body.mass) {
+					UpdateConnectionState();
+				}
+			}
+		}
+
+		private void UpdateConnectionState() {
+			if (connectedBody == previousConnectedBody) {
+				Vector3 connectionMovement = connectedBody.transform.TransformPoint(connectionLocalPosition) - connectionWorldPosition;
+				connectionVelocity = connectionMovement / Time.deltaTime;
+			}
+			connectionWorldPosition = body.position;
+			connectionLocalPosition = connectedBody.transform.InverseTransformPoint(connectionWorldPosition);
+		}
+	
+		private void Jump(Vector3 gravity) {
+			Vector3 jumpDirection;
+			if (OnGround) {
+				jumpDirection = contactNormal;
+			} else if (OnSteep) {
+				jumpDirection = steepNormal;
+				jumpPhase = 0;
+			} else if (maxAirJumps > 0 && jumpPhase <= maxAirJumps) {
+				if (jumpPhase == 0) {
+					jumpPhase = 1;
+				}
+				jumpDirection = contactNormal;
+			} else {
+				return;
+			}
+			stepsSinceLastJump = 0;
+			jumpPhase += 1;
+			float jumpSpeed = Mathf.Sqrt(2f * gravity.magnitude * jumpHeight);
+			jumpDirection = (jumpDirection + upAxis).normalized;
+			float alignedSpeed = Vector3.Dot(velocity, jumpDirection);
+			if (alignedSpeed > 0f) {
+				jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
+			}
+			velocity += jumpDirection * jumpSpeed;
+		}
+
+		private void OnCollisionEnter(Collision collision) {
+			EvaluateCollision(collision);
+		}
+
+		private void OnCollisionStay(Collision collision) {
+			EvaluateCollision(collision);
+		}
+
+		private void EvaluateCollision(Collision collision) {
+			int layer = collision.gameObject.layer;
+			float minDot = GetMinDot(layer);
+			for (int i = 0; i < collision.contactCount; i++) {
+				Vector3 normal = collision.GetContact(i).normal;
+				float upDot = Vector3.Dot(upAxis, normal);
+				if (upDot >= minDot) {
+					groundContactCount++;
+					contactNormal += normal;
+					connectedBody = collision.rigidbody;
+				} else {
+					if (upDot > -0.01f) {
+						steepContactCount += 1;
+						steepNormal += normal;
+						if (groundContactCount == 0) {
+							connectedBody = collision.rigidbody;
+						}
+					}
+					if (desiresClimbing && upDot >= minClimbDotProduct && (climbMask & 1 << layer) != 0) {
+						climbContactCount += 1;
+						climbNormal += normal;
+						lastClimbNormal = normal;
+						connectedBody = collision.rigidbody;
+					}
+				}
+			}
+		}
+
+		private Vector3 ProjectDirectionOnPlane(Vector3 direction, Vector3 normal) {
+			return (direction - normal * Vector3.Dot(direction, normal)).normalized;
+		}
+		
+		private void AdjustVelocity () {
+			float acceleration, speed;
+			Vector3 xAxis, zAxis;
+			if (Climbing) {
+				acceleration = maxClimbAcceleration;
+				speed = maxClimbSpeed;
+				xAxis = Vector3.Cross(contactNormal, upAxis);
+				zAxis = upAxis;
+			} else {
+				acceleration = OnGround ? maxAcceleration : maxAirAcceleration;
+				speed = OnGround && desiresClimbing ? maxClimbSpeed : maxSpeed;
+				xAxis = rightAxis;
+				zAxis = forwardAxis;
+			}
+			xAxis = ProjectDirectionOnPlane(xAxis, contactNormal);
+			zAxis = ProjectDirectionOnPlane(zAxis, contactNormal);
+
+			Vector3 relativeVelocity = velocity - connectionVelocity;
+			float currentX = Vector3.Dot(relativeVelocity, xAxis);
+			float currentZ = Vector3.Dot(relativeVelocity, zAxis);
+
+			float maxSpeedChange = acceleration * Time.deltaTime;
+			float newX = Mathf.MoveTowards(currentX, playerInput.x * speed, maxSpeedChange);
+			float newZ = Mathf.MoveTowards(currentZ, playerInput.y * speed, maxSpeedChange);
+
+			velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
+		}
+		
+		private bool SnapToGround() {
+			if (stepsSinceLastGrounded > 1 || stepsSinceLastJump <= 2) {
+				return false;
+			}
+			float speed = velocity.magnitude;
+			if (speed > maxSnapSpeed) {
+				return false;
+			}
+			if (!Physics.Raycast(body.position, -upAxis, out RaycastHit hit, probeDistance, probeMask)) {
+				return false;
+			}
+			float upDot = Vector3.Dot(upAxis, hit.normal);
+			if (upDot < GetMinDot(hit.collider.gameObject.layer)) {
+				return false;
+			}
+			groundContactCount = 1;
+			contactNormal = hit.normal;
+			float dot = Vector3.Dot(velocity, hit.normal);
+			if (dot > 0f) {
+				velocity = (velocity - hit.normal * dot).normalized * speed;
+			}
+			connectedBody = hit.rigidbody;
+			return true;
+		}
+
+		private float GetMinDot(int layer) {
+			return (stairsMask & 1 << layer) == 0 ? minGroundDotProduct : minStairsDotProduct;
+		}
+		
+		private bool CheckSteepContacts() {
+			if (steepContactCount > 1) {
+				steepNormal.Normalize();
+				float upDot = Vector3.Dot(upAxis, steepNormal);
+				if (upDot >= minGroundDotProduct) {
+					groundContactCount = 1;
+					contactNormal = steepNormal;
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		private bool CheckClimbing() {
+			if (Climbing) {
+				if (climbContactCount > 1) {
+					climbNormal.Normalize();
+					float upDot = Vector3.Dot(upAxis, climbNormal);
+					if (upDot >= minGroundDotProduct) {
+						climbNormal = lastClimbNormal;
+					}
+				}
+				groundContactCount = climbContactCount;
+				contactNormal = climbNormal;
+				return true;
+			}
+			return false;
+		}
+
+		private void OnDrawGizmos() {
+			Vector3 p = transform.position;
+			Vector3 gravity = CustomGravity.GetGravity(p, out Vector3 up);
+			Gizmos.color = Color.green;
+			Gizmos.DrawRay(p, up);
+			Gizmos.color = Color.red;
+			Gizmos.DrawRay(p, gravity * 0.1F);
+		}
+	}
+}
